798e11917953b7a1ce11dde66b51bb4121472d01
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-06-21 23:58:12 -0300

    Install bootstrap and webpacker

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,3 @@
 class Participant < ApplicationRecord
-	
+	belongs_to :room, counter_cache: true
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,8 @@
 class Room < ApplicationRecord
-	
+	before_create :generate_unique_room_identifier
+	has_many :participants, dependent: :destroy
+
+	def generate_unique_room_identifier
+		self.unique_identifier = SecureRandom.hex(5)
+	end
 end
