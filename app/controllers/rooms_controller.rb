5504f2c967722ca4241b5439b2faf8d5e88d29e2
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-06-29 23:04:49 -0300

    Refactor room controller and implement enter room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,6 @@
 class RoomsController < ApplicationController
+	before_action :set_room, only: [ :reveal, :show, :estimate, :reset_room, :enter_room ]
+
 	def new
 		room = Room.new(team_name: params[:team_name])
 		room.save
@@ -23,7 +25,6 @@ class RoomsController < ApplicationController
 			join_room(params[:room_identifier], params[:name])
 		end
 		@user_session_id = session[:session_id]
-		@room = Room.where(unique_identifier: params[:room_identifier]).first
 		@participant = @room.participants.where(user_session_id: @user_session_id).first
 		add_participant_card(@participant) if params[:commit] == "Enter Room"
 		return redirect_to create_participant_path(room_identifier: @room.unique_identifier) if @participant.nil?
@@ -37,30 +38,38 @@ class RoomsController < ApplicationController
 	def estimate
     session_id = session[:session_id]
 
-    room = Room.where(unique_identifier: params[:room_identifier]).first
-
-    participant = room.participants.where(user_session_id: session_id).first
+    participant = @room.participants.where(user_session_id: session_id).first
 		participant.update(estimate: params[:value], can_estimate: false)
 
 		ActionCable.server.broadcast('room_channel', { participant: participant, origin: 'update_estimate' })
   end
 
 	def reset_room
-		room = Room.where(unique_identifier: params[:room_identifier]).first
-		room.update(is_hidden: true)
-		room.participants.update_all(estimate: nil, can_estimate: true)
+		@room.update(is_hidden: true)
+		@room.participants.update_all(estimate: nil, can_estimate: true)
 
-		ActionCable.server.broadcast('room_channel', { origin: 'reset_room', participants_uuids: room.participants.pluck(:uuid) })
+		ActionCable.server.broadcast('room_channel', { origin: 'reset_room', participants_uuids: @room.participants.pluck(:uuid) })
 	end
 
 	def reveal
-		room = Room.where(unique_identifier: params[:room_identifier]).first
-		room.update(is_hidden: false)
+		@room.update(is_hidden: false)
 
-		ActionCable.server.broadcast('room_channel', { participants: room.participants, origin: 'reveal' })
+		ActionCable.server.broadcast('room_channel', { participants: @room.participants, origin: 'reveal' })
+	end
+
+	def enter_room
+		if @room.present?
+			redirect_to create_participant_path(room_identifier: @room.unique_identifier)
+		else
+			redirect_to root_path
+		end
 	end
 
 	private
+
+	def set_room
+		@room = Room.where(unique_identifier: params[:room_identifier]).first
+	end
 
 	def add_participant_card(participant)
 		ActionCable.server.broadcast('room_channel', { participant: participant, origin: 'add_participant_card' })
