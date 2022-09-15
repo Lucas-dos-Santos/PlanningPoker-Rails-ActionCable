6af46ce85a1f45f90c72df995cfc9b01da91aa79
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-09-15 11:55:50 -0300

    Fix url cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://planningpokerfree.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://planningpokerfree.herokuapp.com', 'http://planningpokerfree.herokuapp.com' ]
+  config.action_cable.url = 'wss://planning-poker-free-production.up.railway.app/cable'
+  config.action_cable.allowed_request_origins = [ 'https://planning-poker-free-production.up.railway.app', 'http://planning-poker-free-production.up.railway.app' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
