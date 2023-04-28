6160a9ee08585b56a07bc4c3daf786945f66ba0a
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-04-28 15:35:38 -0300

    fix config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_user
+
+  def set_user
+    participant = Participant.last
+    @current_user = participant.empty? ? nil : participant
+  end
 end
